c25667c6a501f5917135428506de4717ded9c0f3
pratyush-kr <pratyush151198@gmail.com>
2023-07-21 17:20:09 +0530

    Adding allowed hosts

--- crudapp/settings.py
+++ crudapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!un4b_$^!emw3iy@ua5wg3cq)-qc&7^d7=zjukxe^o2#shoc04
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.27.17.66', 'localhost', '127.0.0.1']
 
 
 # Application definition
