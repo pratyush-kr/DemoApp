408c008521bfba57f43eb578e0f9a84e5bce6fb6
pratyush-kr <pratyush151198@gmail.com>
2023-07-21 15:59:05 +0530

    DB Name

--- crudapp/settings.py
+++ crudapp/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'crudapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR + "/db.sqlite3"
+        'NAME': BASE_DIR / '/db.sqlite3',
         # 'ENGINE': 'django.db.backends.mysql',
         # 'NAME': 'demoDb',
         # 'USER': 'demo',
